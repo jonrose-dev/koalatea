4eee9b4bee1c68f194486bf239be9a15944cee57
Jon Rose <jon@jonrose.dev>
2024-01-22 21:11:46 -0500

    Refactor homepage layout to display the logos in a vertical column

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<main className="h-[calc(100vh-12rem)] flex flex-col items-center justify-between p-24">
 			<h1 className="sm:text-5xl text-5xl">Welcome to Koala Tea Coffee</h1>
-			<div className="flex gap-x-4">
+			<div className="flex flex-col pt-4 gap-y-4">
 				<Image src={Logo} className="max-h-96 w-fit" alt="logo" />
 				<Image src={Logo2} className="max-h-96 w-fit" alt="logo" />
 				<Image src={Logo3} className="max-h-96 w-fit" alt="logo" />
