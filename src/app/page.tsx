1cd96921c3890700409a6366e14332b311d35cd5
Jon Rose <jon@jonrose.dev>
2024-01-22 21:19:48 -0500

    Update layout and page styles to fix overflow issues and improve responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Logo3 from "./koalatea3.jpeg";
 
 export default function Home() {
 	return (
-		<main className="h-[calc(100vh-12rem)] flex flex-col items-center justify-between p-24">
+		<main className="h-screen flex flex-col items-center justify-between px-12 sm:px-24 pt-24 overflow-y-auto">
 			<h1 className="sm:text-5xl text-5xl">Welcome to Koala Tea Coffee</h1>
 			<div className="flex flex-col pt-4 gap-y-4">
 				<Image src={Logo} className="max-h-96 w-fit" alt="logo" />
