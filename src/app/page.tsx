64b1684d46e1343ba5bae92a856b37ee5d515aa5
Jon Rose <jon@jonrose.dev>
2024-01-22 21:05:03 -0500

    Add second logo image to the homepage for Koala Tea Coffee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Logo from "./koalatea.jpeg";
+import Logo2 from "./koalatea2.jpeg";
 
 export default function Home() {
 	return (
 		<main className="h-[calc(100vh-12rem)] flex flex-col items-center justify-between p-24">
 			<h1 className="sm:text-5xl text-5xl">Welcome to Koala Tea Coffee</h1>
-			<Image src={Logo} className="max-h-96 w-fit" alt="logo" />
+			<div className="flex gap-x-4">
+				<Image src={Logo} className="max-h-96 w-fit" alt="logo" />
+				<Image src={Logo2} className="max-h-96 w-fit" alt="logo" />
+			</div>
 		</main>
 	);
 }
